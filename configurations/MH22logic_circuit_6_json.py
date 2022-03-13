c945b71045b117aebd4585c223fefaa3b2edb7a3
Malcolm Harper <mharper@crcmail.net>
2022-03-13 15:12:46 -0400

    Restrict the Problem 6 configurations

--- configurations/MH22logic_circuit_6_json.py
+++ configurations/MH22logic_circuit_6_json.py
@@ -6,43 +6,75 @@
 problem = 'MH22logic_circuit_6'
 config_dir = 'configurations'
 
-from pathlib import Path
 import json
-Bnegations_min = 0
-Bnegations_max = 1
-negations_min = 1
-negations_max = 2
+from pathlib import Path
+
+def accept_config(A, C,  B1, B2, gate1, g1o, gate2, g2o, gate3):
+
+# Original configuration yields 160 circuits :-)
+    B_negation_min = 0
+    B_negation_max = 1
+    total_negation_min = 1
+    total_negation_max = 2
+    in_negation_min = 0
+    in_negation_max = 4
+    gate_negation_min = 0
+    gate_negation_max = 2
+    and_gate_min = 0
+    and_gate_max = 3
+
+# New configuration 4 x 2 x 6 = 48 circuits, still a lot
+    B_negation_min = 0
+    B_negation_max = 2
+    total_negation_min = 0
+    total_negation_max = 6
+    in_negation_min = 1
+    in_negation_max = 1
+    gate_negation_min = 1
+    gate_negation_max = 1
+    and_gate_min = 1
+    and_gate_max = 2
+
+    B_negation_count = sum(map(int,(B1[1], B2[1])))
+    in_negation_count = sum(map(int,(A[1], B1[1], B2[1], C[1])))
+    gate_negation_count = sum(map(int,(g1o[1], g2o[1])))
+    total_negation_count = in_negation_count + gate_negation_count
+    and_gate_count = sum(map(int,(gate1[1], gate2[1], gate3[1])))
+    accept = (B_negation_min <= B_negation_count <= B_negation_max) \
+        and (in_negation_min <= in_negation_count <= in_negation_max) \
+        and (gate_negation_min <= gate_negation_count <= gate_negation_max) \
+        and (total_negation_min <= total_negation_count <= total_negation_max) \
+        and (and_gate_min <= and_gate_count <= and_gate_max)
+    return(accept)
 
 configurations = []
 
-for Atogate, Aname, Afunction in [('short','0','$A'),('inline not','1','~$A')]:
-    for Ctogate, Cname, Cfunction in [('short','0','$C'),('inline not','1','~$C')]:
-        for Btogate1, Bname1, Bfunction1 in [('short','0','$B'),('inline not','1','~$B')]:
-            for Btogate2, Bname2, Bfunction2 in [('short','0','$B'),('inline not','1','~$B')]:
-                for gate1, gate1name, gate1function in [('or','0','|'),('and','1','&')]:
-                    for gate1out, gate1outname, gate1outfunction in [('short','0',''),('inline not','1','~')]:
-                        for gate2, gate2name, gate2function in [('or','0','|'),('and','1','&')]:
-                            for gate2out, gate2outname, gate2outfunction in [('short','0',''),('inline not','1','~')]:
-                                for gate3, gate3name, gate3function in [('or','0','|'),('and','1','&')]:
-                                    Bnegations = sum(map(int,(Bname1, Bname2)))
-                                    total_negations = Bnegations + sum(map(int,(Aname, Cname,gate1outname,gate2outname)))
-                                    if (Bnegations_min <= Bnegations <= Bnegations_max) and (negations_min <= total_negations <= negations_max):
+for A in [('short','0','$A'),('inline not','1','~$A')]:                             # Atogate, Aname, Afunction
+    for C in [('short','0','$C'),('inline not','1','~$C')]:                         # Ctogate, Cname, Cfunction
+        for B1 in [('short','0','$B'),('inline not','1','~$B')]:                    # Btogate1, Bname1, Bfunction1
+            for B2 in [('short','0','$B'),('inline not','1','~$B')]:                # Btogate2, Bname2, Bfunction2
+                for gate1 in [('or','0','|'),('and','1','&')]:                      # gate1, gate1name, gate1function
+                    for g1o in [('short','0',''),('inline not','1','~')]:           # gate1out, gate1outname, gate1outfunction
+                        for gate2 in [('or','0','|'),('and','1','&')]:              # gate2, gate2name, gate2function
+                            for g2o in [('short','0',''),('inline not','1','~')]:   # gate2out, gate2outname, gate2outfunction
+                                for gate3 in [('or','0','|'),('and','1','&')]:      # gate3, gate3name, gate3function
+                                    if accept_config(A, C,  B1, B2, gate1, g1o, gate2, g2o, gate3):
                                         configurations.append({
-                                            "filename": Aname+Cname+Bname1+Bname2+gate1name+gate1outname+gate2name+gate2outname+gate3name,
-                                            "Atogate": Atogate,
-                                            "Ctogate": Ctogate,
-                                            "Btogate1": Btogate1,
-                                            "Btogate2": Btogate2,
-                                            "gate1": gate1,
-                                            "gate1out" : gate1out,
-                                            "gate2": gate2,
-                                            "gate2out" : gate2out,
-                                            "gate3": gate3,
-                                            "perl_function": ' '.join(['(',gate1outfunction,'(',Afunction,gate1function,Bfunction1,'))',gate3function,'(',gate2outfunction,'(',Bfunction2,gate2function,Cfunction,'))']),
+                                            "filename": ''.join([A[1], C[1], B1[1], B2[1], gate1[1], g1o[1], gate2[1], g2o[1], gate3[1]]),
+                                            "Atogate": A[0],
+                                            "Ctogate": C[0],
+                                            "Btogate1": B1[0],
+                                            "Btogate2": B2[0],
+                                            "gate1": gate1[0],
+                                            "gate1out" : g1o[0],
+                                            "gate2": gate2[0],
+                                            "gate2out" : g2o[0],
+                                            "gate3": gate3[0],
+                                            "perl_function": ' '.join(['(',g1o[2],'(',A[2],gate1[2],B1[2],'))',gate3[2],'(',g2o[2],'(',B2[2],gate2[2],C[2],'))']),
                                             })
 
 json_out = Path(config_dir) / (problem+'.json')
 
 with open(json_out, "w") as write_file:
-    json.dump(configurations, write_file)
+    json.dump(configurations, write_file, indent=4)
     
